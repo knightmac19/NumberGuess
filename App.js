fc16510f7873c5128b17aa84f7aa96909ddfed57
Patrick Dunn <pmdunn78@gmail.com>
2020-08-02 10:47:40 -0400

    adding custom fonts

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return ( 
+      <AppLoading 
+        startAsync={fetchFonts} 
+        onFinish={() => setDataLoaded(true)} 
+        onError={() => console.log(err)}
+      />
+    );
+  };
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
@@ -16,7 +38,7 @@ export default function App() {
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
-    
+
   };
 
   const gameOverHandler = numOfRounds => {
@@ -26,15 +48,15 @@ export default function App() {
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
   if (userNumber && guessRounds <= 0) {
-    content = 
-      <GameScreen 
-        userChoice={userNumber} 
-        onGameOver={gameOverHandler} 
+    content =
+      <GameScreen
+        userChoice={userNumber}
+        onGameOver={gameOverHandler}
       />
   } else if (guessRounds > 0) {
-    content = 
-      <GameOverScreen 
-        roundsNumber={guessRounds} 
+    content =
+      <GameOverScreen
+        roundsNumber={guessRounds}
         toBeGuessed={userNumber}
         onRestart={configureNewGameHandler}
       />;
